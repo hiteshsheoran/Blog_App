6903bc1d88bfb180655e69e2457984976bfa32de
hitesh <hiteshsheoran100@gmail.com>
2018-02-14 21:01:10 +0530

    structure ready

---
+++ app.js
@@ -0,0 +1,110 @@
+var express         = require("express"),
+    methodOverride  = require("method-override"),
+    expressSantizer = require("express-sanitizer"),
+    mongoose        = require("mongoose"),
+    bodyparser      = require("body-parser"),
+    app             = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(expressSantizer());
+
+app.use(methodOverride("_method"));
+
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+// 	title: "test 1",
+// 	image: "https://images.unsplash.com/reserve/uvRBqDAfQfaGPJiI6lVS_R0001899.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=abde6390b22bc2ecd8baad0217e46d48&dpr=1&auto=format&fit=crop&w=767&h=508&q=60&cs=tinysrgb",
+// 	body: "be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look."
+// });
+
+app.get("/",function(req,res){
+	res.redirect("/blogs");
+});
+
+app.get("/blogs",function(req,res){
+	Blog.find({},function(err,blogs){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("index",{blogs: blogs});
+		}
+	});
+});
+
+app.get("/blogs/new",function(req,res){
+	res.render("new");
+});
+
+//create route
+app.post("/blogs", function(req,res){
+	//1:create blog
+	//req.body.blog.body = req.sanitize(req.body.blog.body);
+	Blog.create(req.body.blog , function(err, newBlog){
+		if(err){
+			res.render("new");
+		} else{
+			//2:redirect to the index
+			res.redirect("/blogs");
+		}
+	});
+});
+
+//show route
+app.get("/blogs/:id", function(req,res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else{
+			res.render("show", {blog: foundBlog});
+		}
+	});
+});
+
+//edit route
+app.get("/blogs/:id/edit", function(req,res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+//update route
+app.put("/blogs/:id",function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err, updateBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+
+// delete route
+app.delete("/blogs/:id", function(req,res){
+	Blog.findByIdAndRemove(req.params.id,function(err){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+});
+
+app.listen(3000,function(){
+	console.log("server is running");
+});
